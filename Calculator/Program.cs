2458d0234435a849b243955a82f8c74aaa0a794c
faisalmelhim <faisalmelhim@gmail.com>
2026-09-23 14:59:00 +0300

    Implemented calculator program

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿/* Inputting code for the calculator app */
+int firstNumber = 15;
+int secondNumber = 4;
+
+int result = firstNumber + secondNumber;
+Console.WriteLine("The result is {0}", result);
+Console.ReadKey();
+
+/* Accepting user input */
+
+Console.WriteLine("Please enter your first number: ");
+int firstNumber_ = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter your second number: ");
+int SecondNumber_ = Convert.ToInt32(Console.ReadLine());
+
+int result_ = firstNumber_ + SecondNumber_;
+Console.WriteLine("The result is {0}", result_);
